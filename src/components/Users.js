b5c105e28398805cccbef88001565f0bb69351cd
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-02-20 22:05:01 +0300

    Users.js codes written

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Users() {
-  return <div>Users</div>;
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        // setLoading(false);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+  });
+  return (
+    <div>
+      <h1>Users</h1>
+
+      {isLoading && <div>Loading...</div>}
+
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
 }
